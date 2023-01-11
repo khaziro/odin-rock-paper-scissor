f796e03ff6a0e30a5d3984bdd0186f7b6cdb1305
Renato <3095948+khaziro@users.noreply.github.com>
2023-01-11 12:37:14 +0200

    Added the logic behind the game

--- script.js
+++ script.js
@@ -1,8 +1,53 @@
 function getComputerChoice() {
-  let choices = ["Paper", "Rock", "Scissors"];
+  let choices = ["paper", "rock", "scissors"];
   const finalChoice = Math.floor(Math.random() * choices.length);
-  console.log(choices[finalChoice]);
   return choices[finalChoice];
 }
 
-getComputerChoice();
+function duelWinner(playerSelection, computerSelection) {
+  playerSelection = prompt(
+    "What do you pick? Rock, paper or scissors."
+  ).toLowerCase();
+  computerSelection = getComputerChoice();
+  let duelResult = null;
+  console.log(playerSelection);
+  console.log(computerSelection);
+
+  if (playerSelection === computerSelection) {
+    duelResult = "Equal!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    duelResult = "Rock beats scissors. You win!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    duelResult = "Rock loses against paper. You lose!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    duelResult = "Scissors beats paper. You win!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "scissors" && computerSelection === "rock") {
+    duelResult = "Scissors lose against rock. You lose!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    duelResult = "Paper beats rock. You win!";
+    console.log(duelResult);
+    return duelResult;
+  }
+  if (playerSelection === "paper" && computerSelection === "scissors") {
+    duelResult = "Paper loses against scissors. You lose!";
+    console.log(duelResult);
+    return duelResult;
+  }
+}
+
+duelWinner();
