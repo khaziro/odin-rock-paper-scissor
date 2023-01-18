4dfb05bc06a7b67a3e5e43b8327957bd714912f6
Renato <3095948+khaziro@users.noreply.github.com>
2023-01-18 23:06:05 +0200

    Added functionality for buttons to stop working when reaching the value wanted and added comments

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+// Variables to keep track of score
 let playerWinCount = 0;
 let computerWinCount = 0;
+
+// Selectors for HTML elements
 let rock = document.querySelector(".rock");
 let scissors = document.querySelector(".scissors");
 let paper = document.querySelector(".paper");
 let resultsParent = document.querySelector(".results_parent");
 
+// Selectors to remove the event listeners when score reaches 5
+let chooseRock = () => game("rock");
+let choosePaper = () => game("paper");
+let chooseScissors = () => game("scissors");
+
+// New elements added with javascript
 const newElement = document.createElement("div");
 const gameScore = document.createElement("div");
-
-newElement.classList.add("results");
 
 // Function to get the computer choice
 function getComputerChoice() {
@@ -26,11 +33,14 @@ function duelWinner(playerSelection, computerSelection) {
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
     resultsParent.appendChild(gameScore);
-    
+
     return playerWinCount, computerWinCount;
   }
   if (playerSelection === "rock" && computerSelection === "scissors") {
-    playerWinCount++;
+    if (playerWinCount < 5) {
+      playerWinCount++;
+    }
+
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou won this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
@@ -39,7 +49,9 @@ function duelWinner(playerSelection, computerSelection) {
     return playerWinCount;
   }
   if (playerSelection === "rock" && computerSelection === "paper") {
-    computerWinCount++;
+    if (computerWinCount < 5) {
+      computerWinCount++;
+    }
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou lost this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
@@ -48,16 +60,20 @@ function duelWinner(playerSelection, computerSelection) {
     return computerWinCount;
   }
   if (playerSelection === "scissors" && computerSelection === "paper") {
-    playerWinCount++;
+    if (playerWinCount < 5) {
+      playerWinCount++;
+    }
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou won this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
     resultsParent.appendChild(gameScore);
-    
+
     return playerWinCount;
   }
   if (playerSelection === "scissors" && computerSelection === "rock") {
-    computerWinCount++;
+    if (computerWinCount < 5) {
+      computerWinCount++;
+    }
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou lost this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
@@ -66,7 +82,9 @@ function duelWinner(playerSelection, computerSelection) {
     return computerWinCount;
   }
   if (playerSelection === "paper" && computerSelection === "rock") {
-    playerWinCount++;
+    if (playerWinCount < 5) {
+      playerWinCount++;
+    }
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou won this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
@@ -75,29 +93,34 @@ function duelWinner(playerSelection, computerSelection) {
     return playerWinCount;
   }
   if (playerSelection === "paper" && computerSelection === "scissors") {
-    computerWinCount++;
+    if (computerWinCount < 5) {
+      computerWinCount++;
+    }
     newElement.textContent = `You choose ${playerSelection}. Computer choose ${computerSelection}.\nYou lost this round!`;
     resultsParent.appendChild(newElement);
     gameScore.textContent = `Player: ${playerWinCount} | Computer: ${computerWinCount}`;
     resultsParent.appendChild(gameScore);
-    
+
     return computerWinCount;
   }
 }
 
 function game(input) {
   duelWinner(input);
-  if (playerWinCount > computerWinCount) {
-    console.log("Player wins!");
+  if (playerWinCount === 5 && playerWinCount > computerWinCount) {
+    gameScore.textContent = `Player wins!`;
   }
-  if (computerWinCount > playerWinCount) {
-    console.log("Computer wins!");
+  if (computerWinCount === 5 && computerWinCount > playerWinCount) {
+    gameScore.textContent = `Computer wins!`;
   }
-  if (computerWinCount === playerWinCount) {
-    console.log("It's equal!");
+  // If statemement to remove add event listener
+  if (computerWinCount === 5 || playerWinCount === 5) {
+    rock.removeEventListener("click", chooseRock);
+    paper.removeEventListener("click", choosePaper);
+    scissors.removeEventListener("click", chooseScissors);
   }
 }
 
-rock.addEventListener("click", () => game("rock"));
-paper.addEventListener("click", () => game("paper"));
-scissors.addEventListener("click", () => game("scissors"));
+rock.addEventListener("click", chooseRock);
+paper.addEventListener("click", choosePaper);
+scissors.addEventListener("click", chooseScissors);
